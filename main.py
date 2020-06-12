8517aaf6d35e8f2faf7c03ce04cb8e37211ef654
Saifullah Jailani <52360551+emperorjailani@users.noreply.github.com>
2020-06-11 23:57:03 -0700

    This is fix for closing bug

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from src.game import *
+import pygame
 WIDTH = 600
 HEIGHT = 800
 while True:
     game = Game(WIDTH,HEIGHT)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
     game.mainMenu()
 
 
